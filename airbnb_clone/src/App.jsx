9288da735b4308c8fcc9a54deadab4c79d84e643
Nuur-R <firdausnuurr@gmail.com>
2023-04-18 14:40:03 +0700

    Airbnb clone

---
+++ airbnb_clone/src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './module/Hero'
+import Navbar from './module/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
