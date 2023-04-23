73b2f490772d7efe3bb32dee7aea120aa465b457
Nuur-R <firdausnuurr@gmail.com>
2023-04-24 04:04:06 +0700

    shoping list firebase

--- airbnb_clone/src/App.jsx
+++ airbnb_clone/src/App.jsx
@@ -1,11 +1,28 @@
+import lData from './localData'
+import Card from './module/Card'
 import Hero from './module/Hero'
 import Navbar from './module/Navbar'
 
+
 function App() {
+  const cards = lData.map(item => {
+    return (
+        <Card 
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+        />
+    )
+})
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      {cards}
     </div>
   )
 }
